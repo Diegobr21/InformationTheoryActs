334157b1b165a6e71927dd976ebec4a0ec8b28cb
Diegobr21 <jdiegobrana@gmail.com>
2021-04-04 12:54:54 -0500

    More preparation for irregular alphabets

--- canales.py
+++ canales.py
@@ -23,13 +23,32 @@ def alfabetos_regulares(inputA, outputB):
         print(f'{i+1}\t{lensA[i]}\t{lensB[i]}')
 
 
-def alfabetos_irregulares(inputA, inputB):
-    print('Ok')
+def alfabetos_irregulares(inputA, inputB, length_c_A):
+    try:
+        n = int(input('Ingrese la longitud hasta la que quiere ver la cant. de combinaciones: '))
+    except:
+        print('Cantidad invalida')
+        sys.exit()
+    print(f'{n} iteraciones para el alfabeto {inputA} y {inputB}')
+    """
+    partitionsA = int((len(inputA)) / length_c_A )#? Particiones en las que hay que dividir el alfabeto de entrada
+    alphabetA = [[] for i in range(partitionsA)]
+    charactersA = [c for c in inputA]
+    for i in range(len(alphabetA)):
+        for j in range(partitionsA):
+            alphabetA[i].append(charactersA[0])
+            charactersA.pop(0)
+    print(alphabetA)
+    """
+
+
+
+
 
 
 
 try:
-    answer = int(input('Ingresa un "1" si el alfabeto de salida es irregular, si no, ingresa otro numero: '))
+    answer = int(input('Ingresa un "1" si el alfabeto de *salida* es irregular, si no, ingresa otro numero: '))
 except:
     print('Ingresa un numero')
     sys.exit()
@@ -43,12 +62,22 @@ if(irregular): #?--IRREGULAR ALPHABETS
         caracteres = int(input('Ingresa cuantos diferentes caracteres hay en el alfabeto de salida: '))
         B = [[] for i in range(caracteres)]
         for i in range(caracteres):
-            caracter = str(input(f'Ingresa el caracter #{1+i} sin espacios: '))
-            B[i].append(caracter)
-        inp_A = str(input('Ingrese los caracteres del alfabeto de entrada sin espacios: '))
-        lengthA = len(inp_A)
+            character = str(input(f'Ingresa el caracter #{1+i} sin espacios: '))
+            B[i].append(character)
+        length_characters_A = int(input('Ingrese la longitud(constante) de los caracteres del alfabeto de entrada: '))
+        lenA = int(input('Ingrese cuantos diferentes caracteres hay en el alfabeto de entrada: '))
+        A = [[] for i in range(lenA)]
+        for i in range(lenA):
+            character = str(input(f'Ingresa el caracter #{1+i} sin espacios: '))
+            if(len(character) == length_characters_A):
+                A[i].append(character)
+            else:
+                print('No corresponde al tamaño especificado')
+                sys.exit()
+        
+        print('Alfabeto de entrada: \n', A)
         print('Alfabeto de salida: \n', B)
-        alfabetos_irregulares(inp_A, B)
+        alfabetos_irregulares(A, B, length_characters_A)
 
     except:
         print('Caracter o numero invalido')
